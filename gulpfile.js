6d2c68eb0cfebedd9199dceaf12a8bb7ff13ac65
Daniel Noyola <pepenoyola13@gmail.com>
2023-02-16 20:14:42 -0600

    Base del proyecto, primer commit

---
+++ gulpfile.js
@@ -0,0 +1,51 @@
+const { src, dest, watch, series, parallel } = require('gulp');
+const sourcemaps = require('gulp-sourcemaps')
+const concat = require('gulp-concat');
+const terser = require('gulp-terser-js');
+const rename = require('gulp-rename');
+const imagemin = require('gulp-imagemin'); // Minificar imagenes 
+const notify = require('gulp-notify');
+const cache = require('gulp-cache');
+const clean = require('gulp-clean');
+const webp = require('gulp-webp');
+
+const paths = {
+    scss: 'src/scss/**/*.scss',
+    js: 'src/js/**/*.js',
+    imagenes: 'src/img/**/*'
+}
+
+function javascript() {
+    return src(paths.js)
+    .pipe(sourcemaps.init())
+    .pipe(concat('bundle.js'))
+    .pipe(terser())
+    .pipe(sourcemaps.write('.'))
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(dest('./build/js'))
+}
+
+function imagenes() {
+    return src(paths.imagenes)
+        .pipe(cache(imagemin({ optimizationLevel: 3 })))
+        .pipe(dest('build/img'))
+        .pipe(notify({ message: 'Imagen Completada' }));
+}
+
+function versionWebp() {
+    return src(paths.imagenes)
+        .pipe(webp())
+        .pipe(dest('build/img'))
+        .pipe(notify({ message: 'Imagen Completada' }));
+}
+
+
+function watchArchivos() {
+    watch(paths.js, javascript);
+    watch(paths.imagenes, imagenes);
+    watch(paths.imagenes, versionWebp);
+}
+
+exports.watchArchivos = watchArchivos;
+exports.imagenes = parallel(imagenes, versionWebp);
+exports.default = parallel(javascript); 

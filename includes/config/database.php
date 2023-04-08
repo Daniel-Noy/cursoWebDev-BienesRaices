41dcb21127b229ddf69f2e3902edd64dbb45d04c
Daniel Noyola <pepenoyola13@gmail.com>
2023-04-08 16:44:07 -0600

    conexion a base de datos editada

--- includes/config/database.php
+++ includes/config/database.php
@@ -1,7 +1,7 @@
 <?php
 // Funcion para importar la conexion a la base de datos a cualquier archivos
 function conectarDB() : mysqli {
-    $db = mysqli_connect('localhost', 'root', 'root', 'bienesraices_crud');
+    $db = mysqli_connect('containers-us-west-167.railway.app', 'root', '7irWKR49Pw6IEisSjBmA', 'railway');
 
     if(!$db) {
         echo "Error, no se pudo conectar a la base de datos";
